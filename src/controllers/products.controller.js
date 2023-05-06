f78d8504174712c5b29ec6a224799cf11b3b8f0a
Oscar Cunduri <oscar_cg22@hotmail.com>
2023-05-06 18:58:36 -0500

    First steps

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,19 @@
+export const createProduct = (req, res) => {
+
+}
+
+export const getProducts = (req, res) => {
+    return res.json('Get products')
+}
+
+export const getProductsById = (req, res) => {
+
+}
+
+export const updateProductsById = (req, res) => {
+
+}
+
+export const deleteProductsById = (req, res) => {
+
+}
